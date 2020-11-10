0a1d18b46edb348d14a6b414edf93e2c44ce5d00
RHU-MAN <73362896+RHU-MAN@users.noreply.github.com>
2020-11-10 19:14:02 +0200

    Create __init__.py

---
+++ hello_c/__init__.py
@@ -0,0 +1,17 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """hello.c exists"""
+    check50.exists("hello.c")
+
+@check50.check(exists)
+def compiles():
+    """hello.c compiles"""
+    check50.c.compile("hello.c", lcs50=True)
+
+@check50.check(compiles)
+def hello():
+    """hello"""
+    check50.run("./hello").exit()
