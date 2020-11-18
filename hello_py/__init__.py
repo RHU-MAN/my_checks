2162d730f9ff8bac0657b5449a55e5417b436dce
RHU-MAN <73362896+RHU-MAN@users.noreply.github.com>
2020-11-18 23:48:02 +0200

    Update __init__.py

--- hello_py/__init__.py
+++ hello_py/__init__.py
@@ -1,11 +1,11 @@
-import check50
+import check50 # import the check50 module
 
-@check50.check()
-def exists():
-    """hello.py exists"""
-    check50.exists("hello.py")
-    
- @check50.check(exists)
-def prints_hello_world():
-    """prints Hello, world!"""
-    check50.run("python3 hello.py").stdout("Hello, world!").exit(0)   
+@check50.check() # tag the function below as check50 check
+def exists(): # the name of the check
+    """description""" # this is what you will see when running check50
+    check50.exists("hello.py") # the actual check
+
+@check50.check(exists) # only run this check if the exists check has passed
+def prints_hello():
+    """prints "hello, world\\n" """
+    check50.run("python3 hello.py").stdout("[Hh]ello, world!?\n", regex=True).exit(0)
