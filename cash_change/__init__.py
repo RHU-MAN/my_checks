94e898f59a1c408e4eaaba72fb298a0b3f1c6f5e
RHU-MAN <73362896+RHU-MAN@users.noreply.github.com>
2020-11-23 15:05:30 +0200

    Update __init__.py

--- cash_change/__init__.py
+++ cash_change/__init__.py
@@ -15,46 +15,28 @@ def compiles():
 
 
 @check50.check(compiles)
-def test041():
-    """input of 0.41 yields output of 4"""
-    check50.run("./cash").stdin("0.41").stdout(coins(4), "4\n").exit(0)
+def test025():
+    """input of 0.25 yields output of 2"""
+    check50.run("./cash").stdin("0.25").stdout(coins(2), "2\n").exit(0)
 
 
 @check50.check(compiles)
-def test001():
-    """input of 0.01 yields output of 1"""
+def test8():
+    """input of 8 yields output of 8"""
     check50.run("./cash").stdin("0.01").stdout(coins(1), "1\n").exit(0)
 
 
 @check50.check(compiles)
-def test015():
-    """input of 0.15 yields output of 2"""
-    check50.run("./cash").stdin("0.15").stdout(coins(2), "2\n").exit(0)
+def test045():
+    """input of 0.45 yields output of 3"""
+    check50.run("./cash").stdin("0.45").stdout(coins(3), "3\n").exit(0)
 
 
 @check50.check(compiles)
-def test160():
-    """input of 1.6 yields output of 7"""
-    check50.run("./cash").stdin("1.6").stdout(coins(7), "7\n").exit(0)
+def test530():
+    """input of 5.30 yields output of 12"""
+    check50.run("./cash").stdin("5.30").stdout(coins(12), "12\n").exit(0)
 
-
-@check50.check(compiles)
-def test230():
-    """input of 23 yields output of 92"""
-    check50.run("./cash").stdin("23").stdout(coins(92), "92\n").exit(0)
-
-
-@check50.check(compiles)
-def test420():
-    """input of 4.2 yields output of 18"""
-    from re import search
-    expected = "18\n"
-    actual = check50.run("./cash").stdin("4.2").stdout()
-    if not search(coins(18), actual):
-        help = None
-        if search(coins(22), actual):
-            help = "did you forget to round your input to the nearest cent?"
-        raise check50.Mismatch(expected, actual, help=help)
 
 
 @check50.check(compiles)
@@ -74,7 +56,8 @@ def test_reject_empty():
     """rejects a non-numeric input of "" """
     check50.run("./cash").stdin("").reject()
 
-
 def coins(num):
     # regex that matches `num` not surrounded by any other numbers (so coins(2) won't match e.g. 123)
     return fr"(?<!\d){num}(?!\d)"
+
+
